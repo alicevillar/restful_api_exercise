c9e6678593bd4e63bd045be3239b8eaa77a52f03
Alice Villar <alsaldanhavillar@gmail.com>
2021-07-23 16:43:03 -0300

    update comments

--- api_fetch.js
+++ api_fetch.js
@@ -3,20 +3,24 @@
 
 // FUNCTION METHOD GET
 
- 
- 
 
-//GET Adam
+//Fetch to GET User Adam. Since Adam does not exist, the expected response is 404
+
 fetch("http://127.0.0.1:5000/user/Adam").then( // then diz: quando a promessa for encaminhada vai fazer a funcao do then
 async(response) => { // funcao async é a função que faz o await 
     const data = await response.json(); //o cod vai travar aqui até o response vir
     console.log(data)
 }
 ) 
+/*
+RESPOSE IN THE CONSOLE: 
 
-// response --> 404 (NOT FOUND)
+404 (NOT FOUND)
 
-//GET Ann
+*/
+ 
+
+//Fetch to GET User Ann. Since Ann does exist, the expected response is a dictionary with Ann's information 
 fetch("http://127.0.0.1:5000/user/Ann").then(
 async(response) => {
     const data = await response.json(); //o cod vai travar aqui até o response vir
@@ -24,11 +28,18 @@ async(response) => {
 }
 ) 
 
- // response --> {name: "Ann", age: 32, occupation: "Doctor"}
+/*
+  response --> 
+  
+  {
+    "name": "Ann",
+    "age": 32,
+    "occupation": "Doctor"
+}
+*/
 
 
-
- // FUNCTION METHOD PUT
+ // FUNCTION METHOD PUT: creates a user if it does not exist it updates user's data
 
 const info_user = new FormData()
 // payload
@@ -45,11 +56,64 @@ fetch("http://127.0.0.1:5000/user/Matheus",{
         console.log(server_response)
     }
 )
-
- // FUNCTION METHOD POST
-
+/*
+RESPOSE =>> {name: "Matheus", age: "90", occupation: "retired"}
+*/
 
  
- // FUNCTION METHOD DELETE
+
+ // FUNCTION METHOD POST: creates a user if it does not exist it returns: "User with name {} already exists"
+
+ //const info_user = new FormData()
+ // payload
+ info_user.append("age","90")
+ info_user.append("occupation","retired")
+ 
+ fetch("http://127.0.0.1:5000/user/Ann",{
+     method: "POST",
+     body: info_user,
+     redirect: "follow"
+ }).then(
+     async(response) => {
+         const server_response = await response.json()
+         console.log(server_response)
+     }
+ )
+ 
+/*
+RESPOSE IN THE CONSOLE: 
+
+POST http://127.0.0.1:5000/user/Ann 400 (BAD REQUEST)
+VM77:11 User with name Ann already exists
+*/
+
+ // FUNCTION METHOD DELETE - deleting user Jason 
 
 
+ fetch("http://127.0.0.1:5000/user/Jason", {method: "DELETE"}) 
+ .then(async(response) => {  
+     const data = await response.json(); //o cod vai travar aqui até o response vir
+     console.log(data)
+ 
+ }) 
+
+ /*
+RESPOSE IN THE CONSOLE: 
+
+Jason is deleted.
+*/
+
+// FUNCTION METHOD GET to Jason just to check if this user has really been deleted
+
+fetch("http://127.0.0.1:5000/user/Jason").then( // then diz: quando a promessa for encaminhada vai fazer a funcao do then
+async(response) => { // funcao async é a função que faz o await
+    const data = await response.json(); //o cod vai travar aqui até o response vir
+    console.log(data)
+}
+) 
+ /*
+RESPOSE IN THE CONSOLE: 
+null
+*/
+ 
+// Response is null, which means the delete method worked :) 
--- main.py
+++ main.py
@@ -25,49 +25,44 @@ users = [
 
 class User(Resource):
  
-    def get(self, name): #precisa passar um argument name. nao passou, entao deu erro
+    def get(self, name): # it is necessary to give the argument for the name.
         for user in users:
             if (name == user["name"]):
                 return user, 200
- 
-    # função só para testar p/ ver se a api está funcionando
-    #def get(self):
-        #return users, 200
+
 
     def post(self, name):
         parser = reqparse.RequestParser()
         parser.add_argument("age")
         parser.add_argument("occupation")
         args = parser.parse_args()
-# verifica se o usuário inserido já existe.
+        # check to see if the user inserted already exists
         for x in users:
             if (name == x["name"]):
                 return "User with name {} already exists".format(name), 400
 
-# aqui vem o ELSE. Ou seja, se o user não existe, então é preciso criá-lo
-        # para criar o usuário, usa a variável user, que recebe um dicionário com todos os argumentos do usuário
-        user = {
+        # here is the ELSE. Thus, if user does not exist, he or she must be created
+         user = {
             "name": name,
             "age": args["age"],
             "occupation": args["occupation"]
         }
         users.append(user)
         return user, 201
-# vai fazer o mesmo q no post e depois vai fazer um for p/ver se o nome já exite.
-    #se já existe vai atualizar os dados
+
     def put(self, name):
         parser = reqparse.RequestParser()
         parser.add_argument("age")
         parser.add_argument("occupation")
         args = parser.parse_args()
 
-        # faz um for para encontrar o usuário. Se encontrou, atualiza dos dados
+        # check if user exists. If it does exist, then it updates the data
         for user in users:
             if (name == user["name"]):
                 user["age"] = args["age"]
                 user["occupation"] = args["occupation"]
                 return user, 200
-        # aqui vem o ELSE. Ou seja, se não encontrou então cria o usuário e coloca na variável user.
+        # here is the ELSE. If user is not found, has to be created and put in this variable
         user = {
             "name": name,
             "age": args["age"],
@@ -79,19 +74,17 @@ class User(Resource):
     def delete(self, name):
         global users
         users = [user for user in users if user["name"] != name]
-# a,b,c,d,. Quero deletar o A. Daí, verifico procurando A na lista. Tudo o que não for A, entra na lista nova.
-        #ou seja, a lista nova não vai contar o A, que portanto terá sido deletado.
-        #Outra forma de escrever isso:
-# varrendo usuário por usuário pelo for e vai inserir os dados se o nome do usuário for diferent
+# Ex.: imagine we have users A,B,C,D,. I want to delete user A. I search for A in the list.
+# The new list will have all the users exepct from user A. Boom! A was deleted :)
+
         return "{} is deleted.".format(name), 200
+        #another way to do this: return f"{name} is deleted", 200
 
-        #return f"{name} is deleted", 200
-
-api.add_resource(User,"/user/<string:name>") 
-
+api.add_resource(User,"/user/<string:name>")
 app.run(debug=True)
 
 '''  
+Another way to do what is in line 76
 elementos = []
 for el in users:
     if el["name"] != name:
